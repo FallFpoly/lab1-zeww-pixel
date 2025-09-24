6a061521160bd5b8e8a471aaf5ed98d0059b998b
ACER <ACER@LAPTOP-22LF0TV8>
2025-09-25 04:50:43 +0700

    Nghia

--- Lab1/src/main/java/com/mycompany/lab1/bai3.java
+++ Lab1/src/main/java/com/mycompany/lab1/bai3.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.lab1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ICT
@@ -12,5 +14,16 @@ public class bai3 {
 
     public static void main(String[] args) {
           // sinh viên code ở đây
+           double SoDien,TienDien;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Nhap so dien:");
+        SoDien=sc.nextDouble();
+        if (SoDien<=50) {
+            TienDien=SoDien*1000;
+            System.out.println("Tien Dien:"+TienDien);
+        } else {
+            TienDien=50*1000+(SoDien-50)*1200;
+            System.out.println("TienDien:"+TienDien);
+        }
     }
 }
